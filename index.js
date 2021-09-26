13441521b3fa9e88fa0d6c8156701f7aae21ccae
dean <diazdean21@gmail.com>
2021-09-26 13:38:50 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+const sinon = 'sinon'
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, names){
+    let result = drivers.filter(string => string.toLowerCase() === names.toLowerCase());
+    return result;
+}
+
+function fuzzyMatch(driver, string) {
+    let letters = driver.filter(beginningLetters => beginningLetters.substring(0,2) === string)
+    return letters; 
+}
+
+function matchName(obj, string){
+    return obj.filter( driver => driver.name === string);
+}
